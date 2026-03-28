a292fa729aacadfac91e2df78554471858f010a9
LucasT <162837058+LucasTS-42034@users.noreply.github.com>
2026-03-27 22:17:31 -0300

    Commit

---
+++ pa/n41.py
@@ -0,0 +1,6 @@
+x = int(1)
+
+while x <= 10:
+     print(str(x))
+     x+=1
+
---
+++ pa/n43.py
@@ -0,0 +1,9 @@
+x = int(0)
+n = int(input("Digite um número: \n"))
+
+while x <= n:
+        print(x)
+        x = x + 1
+
+     
+
