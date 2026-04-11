a4485a89bb4bc393e7e1623f7b4aaa1f2cb90790
Lucas Tedesco <tedescosantoslucas955@gmail.com>
2026-04-10 21:32:44 -0300

    Commt

--- ex100/n58.py
+++ ex100/n58.py
@@ -1,1 +1,21 @@
-while i < 
+st = 0
+soma = int(0)
+i = int(0)
+med = float(0)
+
+while True:
+    n = int(input("Digite um número: "))
+    
+    if n % 2 == 0:
+        soma = soma + n
+        i = i + 1
+        
+    st = input("Continuar? (S ou N) ")
+    
+    if st == "N":
+        if i > 0:
+            med = float(soma / i)
+            print("A média é: " + str(med))
+        else:
+            print("Nenhum número par foi digitado.")
+        break
