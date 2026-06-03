2beec5bdfa997a296475ea1c3a6e65cd899ddccd
Lucas <tedescosantoslucas955@gmail.com>
2026-06-03 20:36:39 -0300

    Comnit

--- ex100/Bloco_7/n61.py
+++ ex100/Bloco_7/n61.py
@@ -3,5 +3,5 @@ i = int(0)
 for i in range(5):
     n = int(input("Digite um número: \n"))
     lis.append(n)
-    i+=1
+
 print(lis)
--- ex100/Bloco_7/n62.py
+++ ex100/Bloco_7/n62.py
@@ -3,7 +3,6 @@ i = int(0)
 for i in range(10):
     n = int(input("Digite um número: \n"))
     lis.append(n)
-    i+=1
 
 soma = sum(lis)
 print("A soma é " + str(soma))
